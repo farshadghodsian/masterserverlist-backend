24f7d0cfa881eb3dffdd2b887e0443536de374ee
Farshad G <farshadgz@gmail.com>
2019-04-02 19:54:56 -0400

    Knex config file now references a secret.js file that stores the credentials and is not checked into source control.

--- app/knex-cfg.js
+++ app/knex-cfg.js
@@ -1,6 +1,19 @@
-//-------------------------------------------------------------------
-//  Config file for KNEX.JS connection including database credentials
-//-------------------------------------------------------------------
+/*-------------------------------------------------------------------
+Config file for KNEX.JS connection including database credentials
+which are stored in a seperate secret.js file with the following format:
+
+module.exports = 
+{
+    host: '[IP of Database Host]',
+    user: '[DB Username]',
+    database: '[DB Name]',
+    password:  '[DB Password]'
+};
+
+This secret.js is added to the .gitignore file so that it is not checked
+into source control.
+-------------------------------------------------------------------*/
+
 var secret = require('./secret');
 
 module.exports = 
